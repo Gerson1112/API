fe0f897060465a8fb10fe0bdb8a7a199fe18198f
Gerson Gil <gersongilfer@gmail.com>
2022-12-12 20:51:04 -0400

    cambios

--- NetCore6.Bl/Validators/TurismoValidator.cs
+++ NetCore6.Bl/Validators/TurismoValidator.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+///////
 namespace NetCore6.Bl.Validators
 {
     public class TurismoValidator : AbstractValidator<TurismoDTO>
